066396c00790e35c03f42eace93cb3e43abdd609
mannydwal209 <105015664+mannydwal209@users.noreply.github.com>
2023-12-04 19:46:17 -0800

    Mroe logic

--- simulator/sim.cpp
+++ simulator/sim.cpp
@@ -1,6 +1,69 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <iomanip>
 
 using namespace std;
+
+#define iType 10011
+#define rType 110011 
+
+//FUNC3 Bits
+//I-type
+#define ADDI 0                   
+#define STLI 10                    
+#define SLTIU 11                   
+#define XORI 100                      
+#define ORI 110                         
+#define ANDI 111                       
+#define SLLI 1              
+#define SRLISRAI 101              
+//R-type
+#define ADDSUB 0              
+#define SLL 1                
+#define SLT 10               
+#define SLTU 011                    
+#define XOR 100             
+#define SRLSRA 101          
+#define OR 110                     
+#define AND 111                   
+
+class imem{
+public:
+    string instruction;
+    string opcode;
+    string rd;
+    string func3;
+    string rs1;
+    string immed;
+    string Rimmed;
+    string rs2;
+};
+
+
+class reg{
+public:
+    int value;
+    bool isSet;
+};
+
+int binaryToDecimal(long n){
+    long temp=n;
+    long dec=0;
+    long base=1;
+
+    while(temp){
+        long last=temp%10;
+        temp=temp/10;
+        dec+=last*base;
+        base*=2;
+    }
+    dec=(dec+128)%256 -128;
+    return dec;
+}
+
 
 void action1() {
     cout << "Performing action 1" << endl;
@@ -20,33 +83,47 @@ void action4() {
 
 int main() {
     char choice;
-    
-    cout << "Select an action:" << endl;
-    cout << "a. Action 1" << endl;
-    cout << "b. Action 2" << endl;
-    cout << "c. Action 3" << endl;
-    cout << "d. Action 4" << endl;
-    cout << "Enter your choice (a/b/c/d): ";
+    bool continueLoop = true;
 
-    cin >> choice;
+    while (continueLoop) {
+        cout << "Select an action:" << endl;
+        cout << "r. RUN" << endl;
+        cout << "s. STEP" << endl;
+        cout << "x(reg). RETURN REG" << endl;
+        cout << "x(addr). RETURN ADR" << endl;
+        cout << "pc. RETURN PC" << endl;
+        cout << "q. QUIT" << endl;
+        cout << "Enter your choice: ";
 
-    switch(choice) {
-        case 'a':
-            action1();
-            break;
-        case 'b':
-            action2();
-            break;
-        case 'c':
-            action3();
-            break;
-        case 'd':
-            action4();
-            break;
-        default:
-            cout << "Invalid choice!" << endl;
-            break;
+        cin >> choice;
+
+        //fetch
+        //decode
+        //execute
+        //
+
+        switch(choice) {
+            case 'r':
+                action1();
+                break;
+            case 's':
+                action2();
+                break;
+            case 'x':
+                action3();
+                break;
+            case 'pc':
+                action4();
+                break;
+            case '1':
+                continueLoop = false; // Set flag to exit loop
+                break;
+            default:
+                cout << "Invalid choice!" << endl;
+                break;
+        }
     }
 
     return 0;
 }
+
